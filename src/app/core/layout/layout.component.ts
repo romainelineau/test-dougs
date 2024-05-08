3f3890a676f6583da59512125c6769b15a69e46f
Romain Elineau <romain.elineau@gmail.com>
2024-05-08 17:06:24 +0200

    create categories component

--- src/app/core/layout/layout.component.ts
+++ src/app/core/layout/layout.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { CategoriesComponent } from 'src/app/categories/categories.component';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [RouterOutlet, NavMenuComponent],
+  imports: [RouterOutlet, NavMenuComponent, CategoriesComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss'
 })
