edf2c530bdec6560afb87260c9deaba7a594eb39
Romain Elineau <romain.elineau@gmail.com>
2024-05-08 22:14:09 +0200

    create category models

---
+++ src/app/categories/models/category.model.ts
@@ -0,0 +1,8 @@
+import { CategoryGroup } from "./category-group.model";
+
+export interface Category {
+    id: number;
+    group?: CategoryGroup;
+    wording: string;
+    description: string;
+}
---
+++ src/app/categories/models/visible-category.model.ts
@@ -0,0 +1,3 @@
+export interface VisibleCategory {
+    id: number;
+}
