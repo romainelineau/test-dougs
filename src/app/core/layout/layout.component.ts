3b012f3cdf25d0dfbb4b45c7ba41046f41a5105b
Romain Elineau <romain.elineau@gmail.com>
2024-05-09 15:44:10 +0200

    add footer

--- src/app/core/layout/layout.component.ts
+++ src/app/core/layout/layout.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { CategoriesComponent } from 'src/app/categories/categories.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [NavMenuComponent, CategoriesComponent],
+  imports: [NavMenuComponent, CategoriesComponent, FooterComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss'
 })
